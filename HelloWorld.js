90f5c7ebd3527009aecf7d9325519ce54f90c6fd
Lily Wang <fwang@wellesley.edu>
2015-04-18 14:41:45 -0400

    changed time display

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,11 +1,24 @@
 function Tick() {
   var Today= new Date()
   var Hours= Today.getHours()
+  Hours = unmilitary(Hours)
   var Seconds= Today.getSeconds()
+  Seconds = addZero(Seconds)
   var Minutes= Today.getMinutes()
+  Minutes = addZero(Minutes)
   $('#button').html(Hours + ":" + Minutes + ":" + Seconds)
 }
-function checkTime(i) {
+
+function addZero(i) {
 if (i<10) {i = "0" + i}; // add zero in front of numbers < 10
 return i;
+}
+
+function unmilitary(hour) {
+  if (hour>12) {
+    return hour-12;
+  }
+  else {
+    return hour
+  }
 }

467ee3b61b94efd7db67d64fa32da543d903148e
Olivia Kotsopoulos <okotsopo@wellesley.edu>
2015-04-18 19:14:54 -0400

    Restoring HelloWorld.html

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,15 +1,4 @@
 function Tick() {
-<<<<<<< HEAD
-  var Today= new Date()
-  var Hours= Today.getHours()
-  var AMPM = getAMPM(Hours)
-  Hours = unmilitary(Hours)
-  var Seconds= Today.getSeconds()
-  Seconds = addZero(Seconds)
-  var Minutes= Today.getMinutes()
-  Minutes = addZero(Minutes)
-  $('#button').html(Hours + ":" + Minutes + ":" + Seconds + " " + AMPM)
-=======
     var Today= new Date();
     var Selected = new Date(Today.getFullYear(), Today.getMonth(), Today.getDate(), military($('#hour').val(), $('#AMPM').val()), $('#minute').val(), 0);
     updateLabel(Today);
@@ -25,7 +14,6 @@ function Tick() {
 function compareTimes () {
   new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 55, 0)
   var now = new Date();
->>>>>>> origin/master
 }
 
 function addZero(i) {
@@ -39,6 +27,20 @@ function military(hour, AMPM) {
     } else {
 	return parseInt(hour) + 12;
     }
+
+}
+
+function getSelectedDate(Today) {
+
+    var year = Today.getFullYear();
+    var month = Today.getFullMonth();
+    var date = Today.getDate();
+    var hour = military($('#hour').val(), $('#AMPM').val());
+    var minute = $('#minute').val();
+
+    if ((hour )
+
+  
 
 }
 
@@ -68,6 +70,17 @@ function startUp() {
 
 function getMinutes() {
   for (minutes= 0; minutes<60; minutes++) {
-  $('#minute').append("<option>"+ addZero(minutes) + "</option>")
+  $('#minute').append("<option value =" + minutes +">"+ addZero(minutes) + "</option>")
   }
+}
+
+function updateLabel(Today) {
+  var Hours= Today.getHours()
+  var AMPM = getAMPM(Hours)
+  Hours = unmilitary(Hours)
+  var Seconds= Today.getSeconds()
+  Seconds = addZero(Seconds)
+  var Minutes= Today.getMinutes()
+  Minutes = addZero(Minutes)
+  $('#button').html(Hours + ":" + Minutes + ":" + Seconds + " " + AMPM)
 }
